7660443bb2f679d3c497cd6b5f78398f69cb0912
Jishan Ahmed <workwithjishan.info@gmail.com>
2024-02-10 21:21:21 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("mongo db is connected");
+});
 
 const app = express();
 
