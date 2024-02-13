124ef477d2a304e0238be3520cc7b948881796bc
Jishan Ahmed <workwithjishan.info@gmail.com>
2024-02-13 21:53:20 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,34 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (request, response) => {
+  const { username, email, password } = request.body;
+
+  //validate user info
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return response.status(400).json({ message: "All fields are required" });
+  }
+
+  //   hashing and salting password
+  const hashPassword = bcryptjs.hashSync(password, 10);
+  //creating user using "User" model
+  const newUser = new User({
+    username,
+    email,
+    hashPassword,
+  });
+
+  try {
+    await newUser.save();
+    response.json("signup successful");
+  } catch (error) {
+    response.status(500).json({ message: error.message });
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running on port: 3000");
@@ -18,3 +20,4 @@ app.listen(3000, () => {
 // /test comes from the user.routes.js
 // and to simplify the code and not to get messy we have created a file (controllers)
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
